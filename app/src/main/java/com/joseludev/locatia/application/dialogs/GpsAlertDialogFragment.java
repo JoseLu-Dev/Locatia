58ed569d61f9b5324904cecad584b5183d281044
JoseLu-Dev <joseludev@gmail.com>
2020-12-16 18:31:00 +0100

    Dialog now closes when it sends you to settings

--- app/src/main/java/com/joseludev/locatia/application/dialogs/GpsAlertDialogFragment.java
+++ app/src/main/java/com/joseludev/locatia/application/dialogs/GpsAlertDialogFragment.java
@@ -4,15 +4,12 @@ import android.app.AlertDialog;
 import android.app.Dialog;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.DialogFragment;
-import androidx.fragment.app.Fragment;
-
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
 
 import com.joseludev.locatia.R;
 
@@ -49,6 +46,7 @@ public class GpsAlertDialogFragment extends DialogFragment {
             Intent callGPSSettingIntent = new Intent(
                     android.provider.Settings.ACTION_LOCATION_SOURCE_SETTINGS);
             startActivity(callGPSSettingIntent);
+            this.dismiss();
         });
 
         builder.setView(view);
