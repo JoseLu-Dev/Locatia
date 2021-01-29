e558e8c3ad78964f9644d9a4deb87a4c55048370
JoseLu-Dev <joseludev@gmail.com>
2021-01-29 17:28:33 +0100

    Distance formatter added

--- app/src/main/java/com/joseludev/locatia/domain/Tools/StringFormatter.java
+++ app/src/main/java/com/joseludev/locatia/domain/Tools/StringFormatter.java
@@ -2,6 +2,9 @@ package com.joseludev.locatia.domain.Tools;
 
 public class StringFormatter {
     public static String distanceFormatter(double distanceKm){
+        if(distanceKm < 0.05){
+            return "<50m";
+        }
         if(distanceKm < 1){
             int distanceM = (int) (distanceKm * 1000);
             return distanceM + "m";
